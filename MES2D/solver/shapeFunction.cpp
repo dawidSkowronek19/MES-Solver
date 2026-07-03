a0d2c11bd9f7e22f4791244001b200f902df3773
Dawid Skowronek <dawidskowronek0@gmail.com>
2026-07-03 11:25:20 +0200

    shapeFunctionUpdate

--- MES2D/solver/shapeFunction.cpp
+++ MES2D/solver/shapeFunction.cpp
@@ -1,11 +1,42 @@
 #include "shapeFunction.hpp"
 
-Position ShapeFunction::bar_to_cartes(double ksi, double eta, int idx) const
+void ShapeFunction::find_accElementPoints(Triangle Element)
 {
-    Triangle triangle = m_grid.getTriangle(idx);
-    Position p1 = m_grid.getPoint(triangle.p1);
-    Position p2 = m_grid.getPoint(triangle.p2);
-    Position p3 = m_grid.getPoint(triangle.p3);
-    
-    double x = 
+    m_accElement={
+        m_grid.getPoint(Element.p1),
+        m_grid.getPoint(Element.p2),
+        m_grid.getPoint(Element.p3)
+    };
+}
+
+Position ShapeFunction::XY(const Position &p1, const Position &p2, const Position &p3) const
+{
+    double x = p1.x() +(p2.x()-p1.x())*m_ksi + (p3.x() - p1.x())*m_eta;
+    double y = p1.y() + (p2.y()-p1.y())*m_ksi + (p3.y() - p1.y())*m_eta;
+
+    return {x,y};
+}
+
+Position ShapeFunction::loc_to_cartes(double ksi, double eta)
+{
+    Position p1 = m_accElement.p1;
+    Position p2 = m_accElement.p2;
+    Position p3 = m_accElement.p3;
+
+    m_ksi=ksi; m_eta = eta;
+    Position r = XY(p1, p2, p3);
+
+    return r;
+}
+
+Position ShapeFunction::loc_to_cartes(double ksi, double eta, Triangle element)
+{
+    Position p1 = m_grid.getPoint(element.p1);
+    Position p2 = m_grid.getPoint(element.p2);
+    Position p3 = m_grid.getPoint(element.p3);
+
+    m_ksi = ksi; m_eta = eta;
+    Position r = XY(p1,p2,p3);
+
+    return r;
 }
--- MES2D/solver/shapeFunction.hpp
+++ MES2D/solver/shapeFunction.hpp
@@ -2,21 +2,26 @@
 #define SHAPE_HPP
 #include "../mesh/Grid.hpp"
 
-struct Element{
-    int idx;
-    Triangle triangle;
-    Position p1
+struct ElementPointPositions{
+    Position p1, p2, p3;
 };
 
 class ShapeFunction{
 
     public:
-        double phi(double ksi ,double eta, int i) const;
+        double phi(double ksi ,double eta, Triangle element) const;
         std::tuple<Position, Position, Position> element_idx;
-        Position bar_to_cartes(double ksi, double eta, int idx) const;
+        void find_accElementPoints(Triangle Element);
+        Position loc_to_cartes(double ksi ,double eta); //with preconditioning, requires find_accElementPoints() before
+        Position loc_to_cartes(double ksi, double eta, Triangle element); //
 
     private:
         const Grid2D& m_grid;
+        ElementPointPositions m_accElement;
+        double m_ksi, m_eta;
+
+        Position XY(const Position &p1, const Position &p2, const Position &p3) const;
+
 
 };
 
